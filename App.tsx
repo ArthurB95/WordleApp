af02c88eb0ed31baa0d49225db7f3b03052822a3
Arthur Bicalho <arthurbic95@gmail.com>
2022-03-02 23:34:21 -0300

    Design Final

--- App.tsx
+++ App.tsx
@@ -1,30 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import {colors} from './src/constants';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import { colors } from "./src/constants";
 
-import Keyboard from './src/components/Keyboard';
+import Keyboard from "./src/components/Keyboard";
+
+const NUMBER_OF_TRIES = 6;
 
 export default function App() {
+  const word = "hello";
+  const letters = word.split("");
+
+  const rows = new Array(NUMBER_OF_TRIES).fill(
+    new Array(letters.length).fill("")
+  );
+
+  const onKeyPressed = (key: string) => {
+    console.warn(key)
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
 
       <Text style={styles.title}>WORDLE</Text>
 
-      <View style={styles.map}>
-        <View style={styles.row}>
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-        </View>
-      </View>
+      <ScrollView showsHorizontalScrollIndicator={false} style={styles.map}>
+        {rows.map((row) => (
+          <View style={styles.row}>
+            {row.map((cell: string) => (
+              <View style={styles.cell}>
+                <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
 
-      <Keyboard />
+      <Keyboard onKeyPressed={onKeyPressed}/>
     </SafeAreaView>
   );
 }
@@ -33,32 +47,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.black,
-    alignItems: 'center',
+    alignItems: "center",
   },
   title: {
     color: colors.lightgrey,
     fontSize: 32,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 7,
-    marginTop: 30
+    marginTop: 30,
   },
   map: {
-    backgroundColor: 'red',
-    alignSelf: 'stretch',
-    height: 100
+    alignSelf: "stretch",
+    marginVertical: 20,
+    height: 100,
   },
 
   row: {
-    alignSelf: 'stretch',
-    flexDirection: 'row',
+    alignSelf: "stretch",
+    flexDirection: "row",
+    justifyContent: "center",
   },
 
   cell: {
-    borderWidth: 2,
+    borderWidth: 3,
     borderColor: colors.darkgrey,
     flex: 1,
     aspectRatio: 1,
-    margin: 3
-  }
+    margin: 3,
+    maxWidth: 70,
+    justifyContent: "center",
+    alignItems: 'center'
+  },
 
+  cellText: {
+    color: colors.lightgrey,
+    fontWeight: "bold",
+    fontSize: 28
+  }
 });
