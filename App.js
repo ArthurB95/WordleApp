cd4ad6d2d3507da9b96b06b9d7920f747b9f827b
Arthur Bicalho <arthurbic95@gmail.com>
2022-03-07 09:50:31 -0300

    Color Commit

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const NUMBER_OF_TRIES = 6;
 
 const copyArray = (arr) => {
   return [...arr.map((rows) => [...rows])];
-}
+};
 
 export default function App() {
   const word = "hello";
@@ -29,7 +29,7 @@ export default function App() {
     if (key === CLEAR) {
       const prevCol = curCol - 1;
       if (prevCol >= 0) {
-        updateRows[curRow][prevCol] = '';
+        updateRows[curRow][prevCol] = "";
         setRows(updateRows);
         setCurCol(prevCol);
       }
@@ -53,10 +53,12 @@ export default function App() {
 
   const isCellActive = (row, col) => {
     return row === curRow && col === curCol;
-  }
+  };
 
-  const getCellBGColor = (letter, row, col) => {
-    if(row >= curRow) {
+  const getCellBGColor = (row, col) => {
+
+    const letter = rows[row][col];
+    if (row >= curRow) {
       return colors.black;
     }
     if (letter === letters[col]) {
@@ -66,7 +68,18 @@ export default function App() {
       return colors.secondary;
     }
     return colors.darkgrey;
+  };
+
+  const getAllLettersWithColor = (color) => {
+
+    return rows.flatMap((row,i) => row.filter((cell, j) => getCellBGColor(i,j) === color))
+
   }
+
+  const greenCaps = getAllLettersWithColor(colors.primary);
+  const yellowCaps = getAllLettersWithColor(colors.secondary);
+  const greyCaps = getAllLettersWithColor(colors.darkgrey);
+
 
   return (
     <SafeAreaView style={styles.container}>
@@ -78,7 +91,18 @@ export default function App() {
         {rows.map((row, i) => (
           <View key={`row-${i}`} style={styles.row}>
             {row.map((letter, j) => (
-              <View key={`cell-${i}-${j}`} style={[styles.cell, { borderColor: isCellActive(i, j) ? colors.lightgrey : colors.darkgrey, backgroundColor: getCellBGColor(letter, i, j) }]}>
+              <View
+                key={`cell-${i}-${j}`}
+                style={[
+                  styles.cell,
+                  {
+                    borderColor: isCellActive(i, j)
+                      ? colors.lightgrey
+                      : colors.darkgrey,
+                    backgroundColor: getCellBGColor(i, j),
+                  },
+                ]}
+              >
                 <Text style={styles.cellText}>{letter.toUpperCase()}</Text>
               </View>
             ))}
@@ -86,7 +110,7 @@ export default function App() {
         ))}
       </ScrollView>
 
-      <Keyboard onKeyPressed={onKeyPressed} />
+      <Keyboard onKeyPressed={onKeyPressed} greenCaps={greenCaps} yellowCaps={yellowCaps} greyCaps={greyCaps}/>
     </SafeAreaView>
   );
 }
@@ -107,7 +131,6 @@ const styles = StyleSheet.create({
   map: {
     alignSelf: "stretch",
     marginVertical: 20,
-    height: 100,
   },
 
   row: {
